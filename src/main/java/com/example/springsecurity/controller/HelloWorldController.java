89ac5ea9f05571009ca031353f82e25dde230ebf
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2025-07-14 21:47:31 -0300

    Implement Spring Security configuration and update application properties

--- src/main/java/com/example/springsecurity/controller/HelloWorldController.java
+++ src/main/java/com/example/springsecurity/controller/HelloWorldController.java
@@ -3,6 +3,9 @@ package com.example.springsecurity.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelloWorldController {
 
@@ -15,5 +18,26 @@ public class HelloWorldController {
     public String noSecHelloWorld() {
         return "Hello, World! sin seguridad";
     }
+
+//    int[] notas = {10, 9, 8, 7, 6, 3, 2, 1};
+//
+//    @GetMapping("/notas")
+//    public List<Integer> notas() {
+//
+//        List<Integer> listaNotas = new ArrayList<>();
+//        for (int nota : notas) {
+//            listaNotas.add(nota);
+//        }
+//        return listaNotas;
+//    }
+//
+//    @GetMapping("/notas/promedio")
+//    public double promedioNotas() {
+//        double suma = 0;
+//        for (int nota : notas) {
+//            suma += nota;
+//        }
+//        return suma / notas.length;
+//    }
 
 }
--- src/main/java/com/example/springsecurity/security/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/config/SecurityConfig.java
@@ -2,9 +2,24 @@ package com.example.springsecurity.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -12,8 +27,52 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests().requestMatchers("/holaNoSeg").
-                permitAll().anyRequest().authenticated().and().formLogin().permitAll().and().httpBasic().and().build();
+        return httpSecurity.csrf(csrf -> csrf.disable()).httpBasic(Customizer.withDefaults())//se usa solo en caso de loguear con user y pass
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(http -> {
+                    //endpoints publicos
+                    http.requestMatchers(HttpMethod.GET, "/holaNoSeg").permitAll();
+                    http.requestMatchers(HttpMethod.GET, "/holaSeg").hasAnyAuthority("READ");
+                    http.anyRequest().denyAll();
+                }).build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        List userDetailsList = new ArrayList<>();
+
+        userDetailsList.add(User.withUsername("esteban").password("123")// esto si no está codificado, sino tendrá que seguir el algoritmo de codificación
+                .roles("ADMIN").authorities("CREATE", "READ", "UPDATE", "DELETE").build());
+
+        userDetailsList.add(User.withUsername("seguidor").password("123").roles("USER").authorities("READ").build());
+
+        userDetailsList.add(User.withUsername("actualizador").password("123").roles("USER").authorities("UPDATE").build());
+
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+
+
+//    @Bean
+//    public SecurityFilterChain apiNotas(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity.authorizeHttpRequests().anyRequest().authenticated().and().formLogin().permitAll().and().build();
+//    }
 
 }
