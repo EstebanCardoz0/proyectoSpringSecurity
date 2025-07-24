4cdbec192102da4c297f5088a4966dfb551b8158
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2025-07-24 16:38:05 -0300

    Add JWT utility class and configuration; implement token creation and update application properties

---
+++ src/main/java/com/example/springsecurity/utils/JwtUtils.java
@@ -0,0 +1,54 @@
+package com.example.springsecurity.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtils {
+
+    //con estas config aseguramos la autenticidad del token a crear
+    @Value("${security.jwt.private.key}")
+    private String privateKey;
+
+    @Value("${security.jwt.user.generator}")
+    private String userGenerator;
+
+    //para encriptar, vamos a necesitar esta clave secreta y este algoritmo
+    public String createToken(Authentication authentication) {
+
+        Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+
+        //esto está dentro del security context holder
+        String username = authentication.getPrincipal().toString();
+
+        //tambien obtenemos los permisos/autorizaciones
+        //la idea es traer los permisos separados por coma
+        String authorities = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        //a partir de esto generamos el token
+        String jwtToken = JWT.create()
+                .withIssuer(this.userGenerator) //usuario que genera el token
+                .withSubject(username)// a quien se le genera el token
+                .withClaim("authorities", authorities)//claims son los datos controlados en el jwt
+                .withIssuedAt(new Date())//fecha generacion del token
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1800000))//fecha expiracion del token en ms
+                .withJWTId(UUID.randomUUID().toString())//id al token - que genere una random
+                .withNotBefore(new Date(System.currentTimeMillis()))//desde cuando es valido (desde ahora en este caso)
+                .sign(algorithm); //nuestra firma es la que creamos con la clave secreta
+
+        return jwtToken;
+
+    }
+
+}
