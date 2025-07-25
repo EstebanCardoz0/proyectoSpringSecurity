f89c6b1afdf10ff541f78e4038a6b6c666ef2199
esteban cardozo <esteban.cardozo.wec@gmail.com>
2025-07-25 11:29:52 -0300

    Implement login functionality in UserDetailsServiceImp; add AuthResponseDTO for response structure and JWT token creation

--- src/main/java/com/example/springsecurity/dto/AuthResponseDTO.java
+++ src/main/java/com/example/springsecurity/dto/AuthResponseDTO.java
@@ -6,5 +6,6 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
  * ciertos métodos como el constructor, los metodos equals (), hashcode() y toString(),
  * basados en los componenetes de datos declarados en la clase
  *  */
+@JsonPropertyOrder({"username", "message", "jwt", "status"})
 public record AuthResponseDTO(String username, String message, String jwt, boolean status) {
 }
--- src/main/java/com/example/springsecurity/service/UserDetailsServiceImp.java
+++ src/main/java/com/example/springsecurity/service/UserDetailsServiceImp.java
@@ -1,13 +1,21 @@
 package com.example.springsecurity.service;
 
+import com.example.springsecurity.dto.AuthLoginRequestDTO;
+import com.example.springsecurity.dto.AuthResponseDTO;
 import com.example.springsecurity.model.UserSec;
 import com.example.springsecurity.repository.IUserRepository;
+import com.example.springsecurity.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -18,6 +26,8 @@ public class UserDetailsServiceImp implements UserDetailsService {
 
     @Autowired
     private IUserRepository userRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
 
     @Override
@@ -52,4 +62,34 @@ public class UserDetailsServiceImp implements UserDetailsService {
                 userSec.isAccountNotLocked(),
                 authorityList);
     }
+
+    public AuthResponseDTO loginUser(AuthLoginRequestDTO authLoginRequest) {
+
+        //recuperamos user y pass
+        String username = authLoginRequest.username();
+        String password = authLoginRequest.pasword();
+
+        Authentication authentication = this.authenticate(username, password);
+        //si todo sale bien
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String accesToken = JwtUtils.createToken(authentication);
+        AuthResponseDTO authResponseDTO = new AuthResponseDTO(username, "login ok", accesToken, true);
+        return authResponseDTO;
+    }
+
+    public Authentication authenticate(String username, String password) {
+
+        //con esto debo buscar el user
+        UserDetails userDetails = this.loadUserByUsername(username);
+
+        if (userDetails == null) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        //si no es igual
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("Invalid password");
+        }
+        return new UsernamePasswordAuthenticationToken(username, userDetails.getPassword(), userDetails.getAuthorities());
+    }
+
 }
